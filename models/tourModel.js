bf8fe69d3881b837a3bd33f1cced9b0e752da4cc
Sơn Hoàng <60213798+SonHoang2@users.noreply.github.com>
2023-11-26 12:44:16 +0700

    add error handling

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         require: [true, 'a tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxLength: [40, 'A tour must have less or equal to 40 characters'],
+        minLength: [10, 'A tour must have more or equal to 10 characters'],
     },
+    slug: String,
     duration: {
         type: Number,
-        required: [true, 'A tour must have a duration']  
+        required: [true, 'A tour must have a duration']
     },
     maxGroupSize: {
         type: Number,
@@ -17,11 +22,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
@@ -31,7 +42,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         require: [true, 'a tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val) {
+                return val < this.price
+            },
+            message: 'Discount price ({VALUE}) should be below regular price'
+        }
+    },
     summery: {
         type: String,
         trim: true,
@@ -52,6 +71,10 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 },
     {
         toJSON: { virtuals: true },
@@ -59,8 +82,25 @@ const tourSchema = new mongoose.Schema({
     }
 )
 
-tourSchema.virtual('durationWeeks').get(function() {
-    return this.duration / 7;
+tourSchema.virtual('durationWeeks').get(function () {
+    return this.duration / 7
+});
+
+// run before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true})
+    next();
+})
+
+tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: { $ne: true }});
+    next();
+})
+
+tourSchema.pre('aggregate', function(next) {
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true }}});
+    console.log(this.pipeline());
+    next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
